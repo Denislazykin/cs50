edb6fa47755d63f03c8aededff8376f66d85494e
Denis Lazykin <dlazykin856@gmail.com>
2022-04-14 21:20:51 +0300

    init comit

---
+++ pset1/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+int main(void)
+{
+        // ask user for input
+        printf("Give me on integer: ");
+        int x = get_int("");
+        printf("Give me another integer: ");
+        int y = get_int("");
+
+        // do the math
+        printf("The sum of %i and %i is %i!\n", x, y, x + y);
+}
