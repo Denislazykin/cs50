6dd8183fbd73ccf3c2b460555f4cb402a82b656b
Denis Lazykin <dlazykin856@gmail.com>
2022-04-15 13:35:11 +0300

    edit hello

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -4,9 +4,9 @@ int main(void)
 {
         // ask user for input
         printf("Give me on integer: ");
-        int x = get_int("");
+        int x = GetInt("");
         printf("Give me another integer: ");
-        int y = get_int("");
+        int y = GetInt("");
 
         // do the math
         printf("The sum of %i and %i is %i!\n", x, y, x + y);
